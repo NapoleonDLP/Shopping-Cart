27a30df81c2ff2a374b959576148695dd66c6caa
NapoleonDLP <napoleon.dlp@gmail.com>
2020-03-20 17:42:27 -0700

    Created plan and outline

--- server/index.js
+++ server/index.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+//TODO: Serve static HTML page aka empty shopping cart
+
+//Routes for shopping list
+//TODO: Create GET for product list
+
+
+//Routes for shopping cart
+//TODO: Create GET display cart items
+
+//TODO: Create PUT to add item to cart
+
+//TODO: Create POST to create a new cart
+
+//TODO: Create DELETE to remove item from cart
+
 
 app.listen(port, () => console.log(`Listening to port:${port}`));
