e62f700a57e05aef75fb343bff0fcc7f11fa5301
NapoleonDLP <napoleon.dlp@gmail.com>
2020-03-27 15:39:58 -0700

    Built /cart  post route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 const products = require('./products.js');
-const db = require('./db.js');
+const Item = require('./db.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -10,7 +14,7 @@ const db = require('./db.js');
 
 //Routes for shopping list
 //TODO: Create GET for product list
-app.get('/product', (req, res) => {
+app.get('/products', (req, res) => {
   res.send(products);
 });
 
@@ -20,7 +24,16 @@ app.get('/product', (req, res) => {
 //TODO: Create PUT to add item to cart
 
 //TODO: Create POST to create a new cart
-
+app.post('/cart', (req, res) => {
+  let item = Item(req.body);
+  item.save((err) => {
+    if (err) {
+      console.error(err);
+      res.send(400);
+    }
+    res.sendStatus(200);
+  });
+});
 //TODO: Create DELETE to remove item from cart
 
 
