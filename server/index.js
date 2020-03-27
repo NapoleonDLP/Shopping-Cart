31bda2a6f8ee3197aec6d3abad00c1568223a5bc
NapoleonDLP <napoleon.dlp@gmail.com>
2020-03-27 11:56:47 -0700

    Imported db

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const products = require('./products.js');
+const db = require('./db.js');
 
 // app.get('/', (req, res) => res.send('Hello World!'));
 
