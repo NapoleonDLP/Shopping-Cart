e471bd6149d90c08d417e8ee3e3a54863fcd6745
NapoleonDLP <napoleon.dlp@gmail.com>
2020-03-30 15:59:15 -0700

    Created App component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+//import "./App.css"; // Imports specific css for this component
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <h1> Hello, world! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
