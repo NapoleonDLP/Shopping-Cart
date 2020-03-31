1904e31a95d519fc66d8ef1fd78d216f1b3d6479
NapoleonDLP <napoleon.dlp@gmail.com>
2020-03-30 17:07:24 -0700

    Adjusted webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
 module.exports = {
   entry: __dirname + '/src/index.jsx',
   // mode: "development",
-  module:{
+  module: {
     rules: [
       {
         test: [/\.jsx$/],
-        exclude:/node_modules/,
-        use{
+        exclude: /node_modules/,
+        use: {
           loader:"babel-loader",
-          options: { presets: ["@babel/env"] }
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"]
+          }
         }
       },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
+      // {
+      //   test: /\.css$/,
+      //   use: ["style-loader", "css-loader"]
+      // }
     ]
   },
   output:{
     filename: 'bundle.js',
-    publicPath: "/dist/"
+    path: __dirname + "/dist"
   }
   // devServer: {
   //   contentBase: path.join(__dirname, "public/"),
