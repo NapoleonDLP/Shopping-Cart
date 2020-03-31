5cdcf52cddc58cfd439f1b6d09d7dd99d52a77b8
NapoleonDLP <napoleon.dlp@gmail.com>
2020-03-31 02:05:47 -0700

    Rendered Shelves componenet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 //import "./App.css"; // Imports specific css for this component
+import Shelves from './Shelves.jsx';
 
 class App extends React.Component {
   render() {
     return(
       <div className="App">
-        <h1> Hello, world! </h1>
+        <h1> Hello, world!</h1>
+        <Shelves />
       </div>
     );
   }
